86c0ed870f59d1ff136736989d0331c2ebea7bc0
winnosuke <vanlinhnguyen0601@gmail.com>
2025-09-22 09:31:07 +0700

    divide

--- GitAssignment/Program.cs
+++ GitAssignment/Program.cs
@@ -6,12 +6,13 @@ class Program
     {
         Console.WriteLine("Calculator Demo");
         Console.WriteLine("1 + 2 = " + Plus(1, 2));
-        Console.WriteLine("Calculator Demo");
-      
+        Console.WriteLine("5 - 3 = " + Minus(5, 3));
+        Console.WriteLine("4 * 3 = " + Multiply(4, 3));
+        Console.WriteLine("10 / 2 = " + Divide(10, 2));
     }
 
-
-
-     static int Minus(int a, int b) => throw new NotImplementedException();
     static int Plus(int a, int b) => a + b;
+    static int Minus(int a, int b) => a - b;
+    static int Multiply(int a, int b) => a * b;
+    static int Divide(int a, int b) => a / b;
 }
