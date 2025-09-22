f875fad276542fc1a2ae5f68bd4ef3bd43b48a5f
winnosuke <vanlinhnguyen0601@gmail.com>
2025-09-22 09:24:27 +0700

    plus

--- GitAssignment/Program.cs
+++ GitAssignment/Program.cs
@@ -5,11 +5,13 @@ class Program
     static void Main()
     {
         Console.WriteLine("Calculator Demo");
+        Console.WriteLine("1 + 2 = " + Plus(1, 2));
+        Console.WriteLine("Calculator Demo");
       
     }
 
 
 
      static int Minus(int a, int b) => throw new NotImplementedException();
- 
+    static int Plus(int a, int b) => a + b;
 }
